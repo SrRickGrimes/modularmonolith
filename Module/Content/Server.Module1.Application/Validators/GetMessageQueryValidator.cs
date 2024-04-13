b88d7087a5da8730fe445a59404dac08936a0d12
SrRickGrimes <81363560+SrRickGrimes@users.noreply.github.com>
2024-04-13 12:52:07 +0900

    Added templates for main project and module
    Added templates for
    Main Project
    Module Project

---
+++ Module/Content/Server.Module1.Application/Validators/GetMessageQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Server.Module1.Contracts.Message.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Module1.Application.Validators
+{
+    internal class GetMessageQueryValidator:AbstractValidator<GetMessageQuery>
+    {
+        public GetMessageQueryValidator()
+        {
+            RuleFor(x=> x.Text).NotEmpty();
+        }
+    }
+}
---
+++ Module/Content/Server.Module1.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Module1.Infrastructure
+{
+    internal static class ServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services) 
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssemblies([typeof(IInfrastructure).Assembly]);
+            });
+            return services;
+        }
+
+        public static IHost UseInfrastructure(this IHost host)
+        {
+            return host;
+        }
+    }
+}
