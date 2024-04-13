30e1dfa32cfc9ff300caad42962bf4a7d7a40c65
SrRickGrimes <81363560+SrRickGrimes@users.noreply.github.com>
2024-04-13 11:07:45 +0900

    removed default module due the limitations of template.json

---
+++ MonolithTemplate/Server.CrossCuttingConcerns/Interfaces/IModuleInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Server.CrossCuttingConcerns.Interfaces
+{
+    public interface IModuleInstaller
+    {
+        public string Name { get; }
+
+        void Install(IServiceCollection services,IConfiguration configuration);
+
+        void Use(IHost host, IConfiguration configuration);
+    }
+}
